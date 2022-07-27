ad8a22ec1275e0d03f4c2f95dcc15017badc875a
Sarthak Makhija <sarthak.makhija@gmail.com>
2022-07-27 12:40:21 +0530

    Sarthak | Adds another workspace test to assert that the system get the latest value of a key

--- db/Workspace_test.go
+++ db/Workspace_test.go
@@ -83,3 +83,48 @@ func TestPut1000KeysValuesAndMultiGetKeysInWorkspace(t *testing.T) {
 		}
 	}
 }
+
+func TestGetTheSameKeyWithTheLatestValue(t *testing.T) {
+	const segmentMaxSizeBytes uint64 = 10 * 1024
+	const bufferMaxSizeBytes uint64 = 16
+
+	directory := tempDirectory()
+	defer os.RemoveAll(directory)
+
+	keyUsing := func(count int) model.Slice {
+		return model.NewSlice([]byte("Key-" + strconv.Itoa(count)))
+	}
+	valueUsing := func(count int) model.Slice {
+		return model.NewSlice([]byte("Value-" + strconv.Itoa(count)))
+	}
+
+	configuration := NewConfiguration(directory, segmentMaxSizeBytes, bufferMaxSizeBytes, comparator.StringKeyComparator{})
+	workspace, _ := newWorkSpace(configuration)
+	for count := 1; count <= 10; count++ {
+		_ = workspace.put(keyUsing(count), valueUsing(count))
+	}
+	for count := 5; count <= 10; count++ {
+		_ = workspace.put(keyUsing(count), valueUsing(2*count))
+	}
+	allowFlushingSSTable()
+
+	keys := []model.Slice{
+		model.NewSlice([]byte("Key-1")),
+		model.NewSlice([]byte("Key-2")),
+		model.NewSlice([]byte("Key-5")),
+		model.NewSlice([]byte("Key-9")),
+	}
+
+	expectedValueByKey := map[string]string{
+		"Key-1": "Value-1",
+		"Key-2": "Value-2",
+		"Key-5": "Value-10",
+		"Key-9": "Value-18",
+	}
+	multiGetResult := workspace.multiGet(keys)
+	for _, result := range multiGetResult {
+		if result.Value.AsString() != expectedValueByKey[result.Key.AsString()] {
+			t.Fatalf("Expected value to be %v, received %v", expectedValueByKey[result.Key.AsString()], result.Value.AsString())
+		}
+	}
+}
